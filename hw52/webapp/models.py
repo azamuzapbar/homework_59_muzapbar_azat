343d6549a8988907a15f625ce11a67cf79f5b471
Azat Muzapbar <azamuzapbar@gmail.com>
2023-02-14 15:26:34 +0600

    Добавил модели, регистрация в админ панели

--- hw52/webapp/models.py
+++ hw52/webapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    CHOICES = (
+        ('new', 'новая'),
+        ('progress', 'процесс'),
+        ('done', 'сделано'),
+    )
+
+    title = models.CharField(verbose_name='название', max_length=200, null=False, blank=False)
+    description = models.TextField(verbose_name='описание', max_length=3000, null=False, blank=False)
+    status = models.CharField(max_length=10, choices=CHOICES, default='new')
+    datetime = models.DateField(auto_now=True, verbose_name='дата изменения')
+
+    def __str__(self):
+        return f'{self.title} - {self.description} - {self.status}'
